4b81fed922d256631521a07d34c45cfad66357be
rohits777 <rohitashinde95@gmail.com>
2020-10-15 10:36:10 +0530

    Added: win or lose is added

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -1,7 +1,17 @@
 class GamblingSimulator{
 public static void main (String args[]){
 int STAKE=100;
-int bet=1
+int bet=1;
 System.out.println( "Welcome to  Gambling program" );
+double gambling=Math.floor(Math.random()*10) % 2;
+if(gambling==1){
+System.out.println("win");
 }
+else{
+System.out.println("lose");
+}
+
+
+}
+
 }
